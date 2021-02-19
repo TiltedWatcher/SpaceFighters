ace865b51e204be592e6004ff6df980f64a4f062
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-02-19 01:07:17 +0100

    Set Up input Manager, scriptable object for game params

---
+++ Assets/Scripts/ModifiableParameters.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameParameters")] 
+
+public class ModifiableParameters : ScriptableObject{
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}

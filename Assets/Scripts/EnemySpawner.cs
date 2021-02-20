eaf2292628f45a7636725d376ca5f6d2a12b5fce
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-02-20 02:21:57 +0100

    Created second wave, created enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour{
+
+    //parameters
+    [SerializeField] List<EnemyWaveConfig> WaveConfigs;
+    int firstWaveIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        var currentWave = WaveConfigs[firstWaveIndex];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(EnemyWaveConfig waveConfig) {
+        for (int enemyCount = 0; enemyCount < waveConfig.getNumberOfEnemies(); enemyCount++) {
+            Instantiate(
+                waveConfig.getEnemyPrefab(),
+                waveConfig.getWaypointsForPath()[0].transform.position,
+                Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.getTimeBetweenSpawns()); 
+        }
+    }
+
+}

ea4832fc72b134fa04a936ec3c6c4fdd9e258ea1
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-02-21 02:54:45 +0100

    Enemy can shoot now, Changed speed and health values on some enemies, set up a way to prevent friendly fire.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,42 +8,72 @@ public class Enemy : MonoBehaviour{
     [SerializeField] float speed;
     [SerializeField] float health;
     [SerializeField] float dmgReductionFromArmor;
+    [SerializeField] float minTimeBetweenShots;
+    [SerializeField] float maxTimeBetweenShots;
+    [SerializeField] float projectileSpeed;
+    [SerializeField] bool isDamagedByFriendlyFire;
+    [SerializeField] GameObject projectile;
+
+
+    //states 
+    [SerializeField] float shotTimer; //serialized for debugging
+
+    void Start(){
+        shotTimer = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        ShootAfterCountDown();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        ProcessHit(other);
+
+    }
+
+    private void ShootAfterCountDown() {
+        shotTimer -= Time.deltaTime;
+        if (shotTimer <= 0) {
+            Fire();
+            shotTimer = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire() {
+        GameObject enemyFire = Instantiate(
+            projectile, 
+            transform.position, 
+            Quaternion.identity);
+        enemyFire.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    private void ProcessHit(Collider2D other) {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (damageDealer.IsPlayerWeapon || isDamagedByFriendlyFire) {
+            float damageDealt = damageDealer.Damage;
+            if (!damageDealer.IgnoresArmor) {
+                damageDealt -= dmgReductionFromArmor;
+            }
+
+            if (damageDealt > 0) { //don't process damage, if damage was reduced to zero, or a negative number by dmg reduction from armor
+                health -= damageDealt;
+                if (health <= 0) {
+                    DestroyThisEnemy();
+                }
+            } 
+        }
+    }
+
+    public void DestroyThisEnemy() {
+        Destroy(gameObject);
+    }
 
     public float Speed {
         get => speed;
     }
     public float Health {
         get => health;
-    }
-
-    void Start(){
-        
-    }
-
-    // Update is called once per frame
-    void Update(){
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
-        float damageDealt = damageDealer.Damage;
-        if (!damageDealer.IgnoresArmor) {
-            damageDealt -= dmgReductionFromArmor;
-        } 
-            
-
-        if (damageDealt > 0) {
-            health -= damageDealt;
-            if (health <= 0) {
-                destroyThisEnemy();
-            }
-        }
-
-        
-    }
-
-    public void destroyThisEnemy() {
-        Destroy(gameObject);
     }
 }
